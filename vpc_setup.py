127ec2cdc0de0b24dbff6cfd7f1c29d2a0a9b0b1
karthikait <135870638+karthikait@users.noreply.github.com>
2023-06-22 17:36:45 +0530

    Update vpc_setup.py

--- vpc_setup.py
+++ vpc_setup.py
@@ -5,7 +5,6 @@ region=sys.argv[1]
 accesskey=sys.argv[2]
 secretkey=sys.argv[3]
 
-# for_vpc = boto3.client('ec2',region_name="ap-south-1",aws_access_key_id="AKIA5B6S4DPLE2B2YG73",aws_secret_access_key="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")
 for_vpc = boto3.client('ec2',region_name=region,aws_access_key_id=accesskey,aws_secret_access_key=secretkey)
 
 vpc_create = for_vpc.create_vpc(
